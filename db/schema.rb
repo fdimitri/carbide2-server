bbaa5ff22386579c74daf1b6866a437f26f083ee
Frank DiMitri <FrankD@frankd.info>
2026-07-17 19:34:50 -0400

    Add client_version column to browser_sessions
    Nullable/additive string the worker stores (not interprets) so session/list can
    surface the creating client's build for a client-side version-mismatch warning.
    
    Refs #42

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_15_000000) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_17_000000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_15_000000) do
   end
 
   create_table "browser_sessions", force: :cascade do |t|
+    t.string "client_version"
     t.datetime "created_at", null: false
     t.jsonb "doc", default: {}, null: false
     t.bigint "forked_from_id"
